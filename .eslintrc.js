bbf32f43168482fa28e9991c36ae3bb5ec011a1a
Sajan Mishra <sjnmishra8@gmail.com>
2018-06-12 15:40:21 +0530

    setup initial react app with cra and apollo-graphql

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'airbnb',
+  globals: {
+    document: 1,
+  },
+  rules: {
+    'react/jsx-filename-extension': 0,
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+
+import ApolloClient from 'apollo-boost';
+
+import registerServiceWorker from './registerServiceWorker';
+import Routes from './routes';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
+registerServiceWorker();
